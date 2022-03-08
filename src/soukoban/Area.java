6ad010a58ebbc12e57d33f8e0200c9d4e010b34f
popendas <sukesuke385@gmail.com>
2022-03-08 14:58:56 +0900

    jaavadocの書き込み

--- src/soukoban/Area.java
+++ src/soukoban/Area.java
@@ -11,14 +11,54 @@ public enum Area {
         this.id = id;
     }
 
+    /**
+     * 自身の文字列表現を戻します
+     *
+     * @return 自身の文字列表現
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * 自身の文字列表現を戻します
+     *
+     * @return 自身の文字列表現
+     */
+    public String toString() {
+        return label;
+    }
+
+    /**
+     * 自身のIDを戻します
+     * - AIR:0
+     * - WALL:1
+     * - BOX:2
+     * - CLEAR_POINT:3
+     * - PLAYER:4
+     * - MIX_MARK:5
+     * - CLEAR_MARK:6
+     *
+     * @return 自身のID
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * 渡された整数と同じIDを持つAreaオブジェクトを戻します。
+     * - 0:AIR
+     * - 1:WALL
+     * - 2:BOX
+     * - 3:CLEAR_POINT
+     * - 4:PLAYER
+     * - 5:MIX_MARK
+     * - 6:CLEAR_MARK
+     *
+     * @param id 渡す整数
+     * @return 渡された整数と同じIDを持つAreaオブジェクト
+     * @throws IllegalArgumentException 渡された整数に対するAreaオブジェクトがないとき
+     */
     public static Area getById(int id) {
 
         for (Area area : Area.values()) { //拡張for文による走査
